046bfb56f4259b29bf24f0f6f77c37d7cee09a07
JP Meyer <meyer.jayps@gmail.com>
2014-06-27 12:28:19 +0200

    Started on settings page

---
+++ components/com_gotauction/controller.php
@@ -0,0 +1,12 @@
+<?php
+
+defined ('_JEXEC') or die();
+
+class GotauctionController extends JControllerLegacy
+{
+	function __construct()
+	{
+		parent::__construct();		
+		//check view vs user permissions here
+	}
+}
---
+++ components/com_gotauction/views/settings/tmpl/default.php
@@ -0,0 +1,14 @@
+<?php 
+	defined ("_JEXEC") or die(); 
+	JHtml::_('bootstrap.framework');
+	JHtml::_('script', 'system/core.js', false, true);
+	
+	jQuery(document).ready(function()
+	{
+		//load settings from the database if there are any
+		
+	});
+?>
+
+<h1>Gotauction Settings</h1>
+
