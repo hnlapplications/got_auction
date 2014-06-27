6f59874f16661bc7179f36931c1c9e41f608ca56
JP Meyer <meyer.jayps@gmail.com>
2014-06-27 13:24:56 +0200

    Commit from JP pc...

--- components/com_gotauction/views/settings/tmpl/default.php
+++ components/com_gotauction/views/settings/tmpl/default.php
@@ -3,12 +3,17 @@
 	JHtml::_('bootstrap.framework');
 	JHtml::_('script', 'system/core.js', false, true);
 	
+	
+?>
+
+<script type="text/javascript">
 	jQuery(document).ready(function()
 	{
 		//load settings from the database if there are any
 		
 	});
-?>
+</script>
+
 
 <h1>Gotauction Settings</h1>
 
--- tmp/install_53ad1f2309a93/admin/models/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined ("_JEXEC") or die();
-
-class GotauctionModelHome extends JModelList
-{
-	//no functionality necessary here
-}
