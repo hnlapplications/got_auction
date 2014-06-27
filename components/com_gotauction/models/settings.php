d6bba31cec9e30036c62e802cf159886be109faf
JP Meyer <meyer.jayps@gmail.com>
2014-06-27 13:12:08 +0200

    Modified database installer to include table prefix in names.

---
+++ components/com_gotauction/models/settings.php
@@ -0,0 +1,9 @@
+<?php defined ('_JEXEC') or die();
+
+class GotauctionModelSettings extends JModelList
+{	
+	public function __construct($config=array())
+	{		
+		parent::__construct($config);
+	}
+}
