7dc87210eeb669bdb75890646ac88d150037ae4a
Lloyd <ldmckeen@gmail.com>
2014-06-30 09:20:58 +0200

    Update SQL

--- administrator/components/com_gotauction/sql/install.mysql.utf8.sql
+++ administrator/components/com_gotauction/sql/install.mysql.utf8.sql
@@ -17,130 +17,29 @@ CREATE TABLE IF NOT EXISTS `#__gotauction_address` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `auction`
---
-
-CREATE TABLE IF NOT EXISTS `#__gotauction_auction` (
-  `id` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `auction_id` int(11) NOT NULL,
-  `auction_type` int(11) NOT NULL,
-  `auctioneer` varchar(100) NOT NULL,
-  `auction_category` int(11) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `address` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `time` time NOT NULL,
-  `viewing` int(1) NOT NULL DEFAULT '0',
-  `viewing_date` date NOT NULL,
-  `viewing_time` time NOT NULL,
-  `terms` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `auctioneer`
---
-
-CREATE TABLE IF NOT EXISTS `#__gotauction_auctioneer` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `company` varchar(100) NOT NULL,
-  `contact_no` int(11) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `profile_image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `auction_category`
---
-
-CREATE TABLE IF NOT EXISTS `#__gotauction_auction_category` (
-  `id` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `auction_type`
---
-
-CREATE TABLE IF NOT EXISTS `#__gotauction_auction_type` (
-  `id` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lot`
---
-
-CREATE TABLE IF NOT EXISTS `#__gotauction_lot` (
-  `id` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `auction_id` int(11) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `lot_type` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `vat` int(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lot_type`
---
-
-CREATE TABLE IF NOT EXISTS `#__gotauction_lot_type` (
-  `id` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `settings`
---
-
-CREATE TABLE IF NOT EXISTS `#__gotauction_settings` (
-  `id` int(11) NOT NULL,
-  `company_name` varchar(100) NOT NULL,
-  `director` varchar(100) NOT NULL,
-  `logo` varchar(100) NOT NULL,
-  `contact_no` int(11) NOT NULL,
-  `address` int(11) NOT NULL,
-  `email` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-=======
---
--- Table structure for table `address`
+-- Table structure for table `#__gotauction_address`
 --
 
 CREATE TABLE IF NOT EXISTS `#__gotauction_address` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `street_number` int(11) NOT NULL,
   `street_name` varchar(100) NOT NULL,
   `suburb` varchar(100) NOT NULL,
   `city` varchar(100) NOT NULL,
   `post_code` int(11) NOT NULL,
   `gps_x` float NOT NULL,
-  `gps_y` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `gps_y` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `auction`
+-- Table structure for table `#__gotauction_auction`
 --
 
 CREATE TABLE IF NOT EXISTS `#__gotauction_auction` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   `auction_id` int(11) NOT NULL,
   `auction_type` int(11) NOT NULL,
@@ -153,87 +52,97 @@ CREATE TABLE IF NOT EXISTS `#__gotauction_auction` (
   `viewing` int(1) NOT NULL DEFAULT '0',
   `viewing_date` date NOT NULL,
   `viewing_time` time NOT NULL,
-  `terms` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `terms` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `auctioneer`
+-- Table structure for table `#__gotauction_auctioneer`
 --
 
 CREATE TABLE IF NOT EXISTS `#__gotauction_auctioneer` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `company` varchar(100) NOT NULL,
   `contact_no` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `profile_image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `profile_image` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `auction_category`
+-- Table structure for table `#__gotauction_auction_category`
 --
 
 CREATE TABLE IF NOT EXISTS `#__gotauction_auction_category` (
-  `id` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `auction_type`
+-- Table structure for table `#__gotauction_auction_type`
 --
 
 CREATE TABLE IF NOT EXISTS `#__gotauction_auction_type` (
-  `id` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lot`
+-- Table structure for table `#__gotauction_lot`
 --
 
 CREATE TABLE IF NOT EXISTS `#__gotauction_lot` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   `auction_id` int(11) NOT NULL,
   `description` varchar(500) NOT NULL,
   `lot_type` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `vat` int(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `vat` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lot_type`
+-- Table structure for table `#__gotauction_lot_type`
 --
 
 CREATE TABLE IF NOT EXISTS `#__gotauction_lot_type` (
-  `id` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `settings`
+-- Table structure for table `#__gotauction_settings`
 --
 
 CREATE TABLE IF NOT EXISTS `#__gotauction_settings` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `company_name` varchar(100) NOT NULL,
   `director` varchar(100) NOT NULL,
   `logo` varchar(100) NOT NULL,
   `contact_no` int(11) NOT NULL,
   `address` int(11) NOT NULL,
-  `email` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+
 
 >>>>>>> 0e20315f6f7ce8c5520868ff5b9cc2927a8627b4
